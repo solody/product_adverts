9d24bf0226c2c25e08c6febc724a748239f67a22
Kent <164713332@qq.com>
2019-02-26 20:41:02 +0800

    add swiper block

---
+++ src/Plugin/Block/AdvertsSwiper.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\product_adverts\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+/**
+ * Provides a 'AdvertsSwiper' block.
+ *
+ * @Block(
+ *  id = "adverts_swiper",
+ *  admin_label = @Translation("Adverts swiper"),
+ * )
+ */
+class AdvertsSwiper extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+  /**
+   * Constructs a new AdvertsSwiper object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['adverts_swiper']['#markup'] = 'Implement AdvertsSwiper.';
+
+    return $build;
+  }
+
+}
