3caeb96e125b31669818361466a7be22548d9d97
Kent <164713332@qq.com>
2019-02-28 12:19:47 +0800

    fix js files format

--- asset/adverts-swiper.js
+++ asset/adverts-swiper.js
@@ -1,12 +1,19 @@
-var swiper = new Swiper('.swiper-container', {
-  autoHeight: true, //enable auto height
-  spaceBetween: 20,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+(function ($, Drupal) {
+  Drupal.behaviors.initAdvertsSwiper = {
+    attach: function (context, settings) {
+      var swiper = new Swiper('.swiper-container', {
+        autoHeight: true, //enable auto height
+        spaceBetween: 20,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+      console.log(swiper);
+    }
+  };
+})(jQuery, Drupal);
